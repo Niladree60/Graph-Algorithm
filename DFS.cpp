9f176f900ff2e3111867c728dc820d1b459daa8b
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2022-04-04 15:27:26 +0600

    Create DFS.cpp

---
+++ DFS.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+ll vis[200005];
+vector<ll> adj[200005];
+
+void dfs(ll v)
+{
+  vis[v] = 1;
+  cout << v << " ";
+  for(int child : adj[v])
+  {
+    if(vis[child] == 0)
+    {
+      dfs(child);
+    }
+  }
+}
+
+void solve()
+{
+  ll n;
+  cin >> n;
+  for(int i = 1 ; i <= n-1 ; i++)
+  {
+    ll a,b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+  dfs(1);
+  
+}
+
+int main() 
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("ou.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    /*ll tc;
+    cin >> tc;
+    while(tc--)
+    {
+      solve();  
+    }*/
+    solve();
+    
+    return 0;
+}
